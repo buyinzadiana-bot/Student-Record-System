21b05443e3d5214b3859fdb490e121291500831a
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-08 11:38:33 +0200

    read from file

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -1,2 +1,24 @@
+import java.io.BufferedReader;
+
 public class StudentManager {
+
+    private List<Student> students = new ArrayList<>();
+
+//    Read from file
+
+    public void loadFromFile( String FileName){
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+
+                if (parts.length != 4){
+                    throw new IllegalArgumentException("Invalid data format");
+
+                }
+
+            }
+        }
+    }
 }
