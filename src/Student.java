80d4b00d3e16865f89a8548e1f529584a79523f5
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-08 10:33:24 +0200

    initialized a Student object by assigning real values to its fields.

--- src/Student.java
+++ src/Student.java
@@ -4,6 +4,14 @@ public class Student {
     private int age;
     private int score;
 
+    public Student(int id, String name, int age, int score){
+
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.score = score;
+
+    }
 
 
 }
